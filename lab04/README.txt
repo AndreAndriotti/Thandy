540c86f6a7ce77231c27e59408b921ed684b2427
Thayna <32092229@mackenzista.com.br>
2022-04-17 16:33:38 -0300

    criando processos pai-filho e acessando/modificando a variavel compartilhada

---
+++ lab04/README.txt
@@ -0,0 +1,22 @@
+
+>>> Como compilar o programa na plataforma disponibilizada
+
+    Acesse a pasta Thandy/lab04.
+    Para compilar o programa, utilizamos o gcc. 
+
+        $ gcc lab04.c -o lab04
+
+>>> Como executar o programa
+
+    $ ./lab04
+
+
+>>> Como comprovar que os resultados propostos foram alcançados.
+
+    Execução do código:
+
+    Variavel compartilhada: 1
+    Childpid: 23106
+    Childpid: 0
+    [Filho] Variavel compartilhada (+2): 3
+    [Pai] Variavel compartilhada (*4): 12
--- lab04/lab04.c
+++ lab04/lab04.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/shm.h>
+#include <sys/wait.h>
 
 int main(){
   
@@ -23,6 +24,25 @@ int main(){
     int *value = (int*)shmat(shm_id, NULL, 0);
     *value = 1;
     printf("Variavel compartilhada: %i\n",*value);
+
+    // fork(): função do processo pai que cria um processo filho
+    child_pid = fork();
+    printf("Childpid: %i\n",child_pid);
+    
+    if (child_pid == 0)
+    {     
+        *value+= 2;  
+
+        printf("[Filho] Variavel compartilhada (+2): %i\n",*value);    
+    }
+    else
+    {
+        wait(NULL);     
+        *value *= 4 ;
+
+        printf("[Pai] Variavel compartilhada (*4): %i\n",*value);    
+    }
+  
     
     return 0;
 }
